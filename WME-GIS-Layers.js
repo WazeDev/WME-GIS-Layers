53a3927c97ac843508599e648c9246563ec1939d
MapOMatic <mapomatic.waze@gmail.com>
2019-01-05 11:26:55 -0500

    correct version #

--- WME-GIS-Layers.js
+++ WME-GIS-Layers.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         WME GIS Layers
 // @namespace    https://greasyfork.org/users/45389
-// @version      2018.01.05.001
+// @version      2019.01.05.001
 // @description  Adds GIS layers in WME
 // @author       MapOMatic
 // @include      /^https:\/\/(www|beta)\.waze\.com\/(?!user\/)(.{2,6}\/)?editor\/?.*$/
