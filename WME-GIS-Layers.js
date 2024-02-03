593657cda1846bec1a42c8a0f4018a1f7aa36d58
MapOMatic <mapomatic.waze@gmail.com>
2024-02-03 11:00:45 -0500

    bump version and add a release note alert

--- WME-GIS-Layers.js
+++ WME-GIS-Layers.js
@@ -3,7 +3,7 @@
 // ==UserScript==
 // @name         WME GIS Layers
 // @namespace    https://greasyfork.org/users/45389
-// @version      2023.12.12.001
+// @version      2024.02.03.001
 // @description  Adds GIS layers in WME
 // @author       MapOMatic
 // @match         *://*.waze.com/*editor*
@@ -1311,11 +1311,11 @@
     const DEFAULT_VISIBLE_AT_ZOOM = 18;
     const SETTINGS_STORE_NAME = 'wme_gis_layers_fl';
     const COUNTIES_URL = 'https://tigerweb.geo.census.gov/arcgis/rest/services/Census2020/State_County/MapServer/1/';
-    const ALERT_UPDATE = false;
+    const ALERT_UPDATE = true;
     const SCRIPT_NAME = GM_info.script.name;
     const SCRIPT_VERSION = GM_info.script.version;
     const DOWNLOAD_URL = 'https://greasyfork.org/scripts/369632-wme-gis-layers/code/WME%20GIS%20Layers.user.js';
-    const SCRIPT_VERSION_CHANGES = [];
+    const SCRIPT_VERSION_CHANGES = ['Add a shortcut key to allow toggling of GIS-L overlay. Find it under the "Layers" group in the Shortcut Keys.'];
     let _mapLayer = null;
     let _roadLayer = null;
     let _settings = {};
