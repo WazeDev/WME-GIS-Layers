21b1e4bb4dd3aaf3326a70dc75c032cdcc90fa43
MapOMatic <mapomatic.waze@gmail.com>
2025-04-23 13:31:24 -0400

    Bump version

--- WME-GIS-Layers.js
+++ WME-GIS-Layers.js
@@ -3,7 +3,7 @@
 // ==UserScript==
 // @name         WME GIS Layers
 // @namespace    https://greasyfork.org/users/45389
-// @version      2025.04.20.001
+// @version      2025.04.23.000
 // @description  Adds GIS layers in WME
 // @author       MapOMatic
 // @match         *://*.waze.com/*editor*
